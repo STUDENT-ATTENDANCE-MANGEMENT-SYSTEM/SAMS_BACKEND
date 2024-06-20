959e4856b3235485c65d5f0d856633bfc05d3cee
s.segunshowunmi@gmail.com <s.segunshowunmi@gmail.com>
2024-06-20 14:17:24 -0700

    deploy

--- src/controllers/attendanceController.js
+++ src/controllers/attendanceController.js
@@ -6,25 +6,27 @@ import asyncHandler from "express-async-handler";
 // @route GET /attendances
 // @access Private
 const getAllAttendances = asyncHandler(async (req, res) => {
-   // Assuming you're passing these as query parameters
+  // Assuming you're passing these as query parameters
   try {
     const { attendanceTabId, lecturerId } = req.params;
     if (!attendanceTabId || !lecturerId) {
-      return res.status(400).send({ message: 'attendanceTabId and lecturerId are required' });
+      return res
+        .status(400)
+        .send({ message: "attendanceTabId and lecturerId are required" });
     }
 
     // Query the database for attendance records matching the criteria
     const attendanceRecords = await Attendance.find({
       attendanceTabId: attendanceTabId,
-      lecturerId: lecturerId
+      lecturerId: lecturerId,
     });
 
     // Send the found attendance records as a response
     res.status(200).json(attendanceRecords);
   } catch (error) {
     // Handle possible errors
-    console.error('Error fetching attendance records:', error);
-    res.status(500).send({ message: 'Error fetching attendance records' });
+    console.error("Error fetching attendance records:", error);
+    res.status(500).send({ message: "Error fetching attendance records" });
   }
 });
 
@@ -57,7 +59,10 @@ const createAttendance = asyncHandler(async (req, res) => {
   const { lecturerId, attendanceTabId } = req.body;
   try {
     // Step 1: Create a new Attendance record
-    const newAttendance = await Attendance.create({ lecturerId, attendanceTabId });
+    const newAttendance = await Attendance.create({
+      lecturerId,
+      attendanceTabId,
+    });
 
     // Step 2: MongoDB automatically generates an _id, which we use as attendanceId
     const attendanceId = newAttendance._id;
@@ -69,12 +74,15 @@ const createAttendance = asyncHandler(async (req, res) => {
       { new: true } // Return the updated document
     );
 
-    res.status(201).send({ message: 'Attendance created and AttendanceTab updated', attendanceId });
+    res.status(201).send({
+      message: "Attendance created and AttendanceTab updated",
+      attendanceId,
+    });
   } catch (error) {
-    console.error('Error creating attendance:', error);
-    res.status(500).send({ message: 'Failed to create attendance' });
+    console.error("Error creating attendance:", error);
+    res.status(500).send({ message: "Failed to create attendance" });
   }
-})
+});
 
 const updateStudentAttendance = asyncHandler(async (req, res) => {
   const { attendanceId, lecturerId, name, department, matricNumber } = req.body;
@@ -92,23 +100,26 @@ const updateStudentAttendance = asyncHandler(async (req, res) => {
   }
 
   if (attendance.lecturerId.toString() !== lecturerId) {
-    return res.status(403).json({ message: "Unauthorized: You are not allowed to update this attendance" });
+    return res.status(403).json({
+      message: "Unauthorized: You are not allowed to update this attendance",
+    });
   }
 
   // Find the student in the students array
 
-    attendance.students.name = name;
-    attendance.students.department = department;
-    attendance.students.matricNumber = matricNumber;
-  
-    // Add a new student to the array
-    attendance.students.push({ name, department, matricNumber });
-  
+  attendance.students.name = name;
+  attendance.students.department = department;
+  attendance.students.matricNumber = matricNumber;
+
+  // Add a new student to the array
+  attendance.students.push({ name, department, matricNumber });
 
   // Save the updated attendance document
   await attendance.save();
 
-  res.status(200).json({ message: "Attendance updated successfully", attendance });
+  res
+    .status(200)
+    .json({ message: "Attendance updated successfully", attendance });
 });
 
 const updateAttendance = asyncHandler(async (req, res) => {
@@ -127,7 +138,9 @@ const updateAttendance = asyncHandler(async (req, res) => {
   }
 
   // Find the student in the students array
-  const studentIndex = attendance.students.findIndex(student => student.studentId === studentId);
+  const studentIndex = attendance.students.findIndex(
+    (student) => student.studentId === studentId
+  );
 
   if (studentIndex !== -1) {
     // Update existing student's details
@@ -142,7 +155,9 @@ const updateAttendance = asyncHandler(async (req, res) => {
   // Save the updated attendance document
   await attendance.save();
 
-  res.status(200).json({ message: "Attendance updated successfully", attendance });
+  res
+    .status(200)
+    .json({ message: "Attendance updated successfully", attendance });
 });
 
 const deleteAttendance = asyncHandler(async (req, res) => {
@@ -172,7 +187,9 @@ const deleteStudentFromAttendance = asyncHandler(async (req, res) => {
 
   // Confirm both attendanceId and studentId are provided
   if (!attendanceId || !studentId) {
-    return res.status(400).json({ message: "Attendance ID and Student ID are required" });
+    return res
+      .status(400)
+      .json({ message: "Attendance ID and Student ID are required" });
   }
 
   // Find the attendance document
@@ -183,7 +200,9 @@ const deleteStudentFromAttendance = asyncHandler(async (req, res) => {
   }
 
   // Find the index of the student in the students array
-  const studentIndex = attendance.students.findIndex(student => student._id.toString() === studentId);
+  const studentIndex = attendance.students.findIndex(
+    (student) => student._id.toString() === studentId
+  );
 
   // If student exists, remove them
   if (studentIndex !== -1) {
@@ -202,5 +221,5 @@ export default {
   updateStudentAttendance,
   deleteAttendance,
   createAttendance,
-  deleteStudentFromAttendance
+  deleteStudentFromAttendance,
 };
